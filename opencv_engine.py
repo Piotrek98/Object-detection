e491252e583f14108c7f04b6c111bd3978f892a9
Piotrek98 <p.szewczyk-98@wp.pl>
2019-05-29 23:24:25 +0200

    add text to rectangle

--- opencv_engine.py
+++ opencv_engine.py
@@ -1,31 +1,35 @@
 import cv2
 import sys
 
-class HaarClassifier():
+class HaarClassifier:
     def __init__(self):
         self.faceCascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
     def detectFace(self, grayFrame, outputFrame):
-        scaleFactor = 1.15
+        scaleFactor = 1.2
         minNeighbors = 5
         faces = self.faceCascade.detectMultiScale(grayFrame, scaleFactor, minNeighbors)
         for (x, y, w, h) in faces:
-            cv2.rectangle(outputFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            outlined_image = cv2.rectangle(outputFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(outlined_image, 'Face', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
 
-count = 0
-class Recognize(HaarClassifier):
+class Recognition:
     def __init__(self):
         super().__init__()
-        self.haarClassifier = HaarClassifier
+        self.haarClassifier = HaarClassifier()
 
     def capture(self):
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(0) #0 for online camera
 
-        if cap.isOpened():
-            return cap
-        else:
-            print('Failed')
-            sys.exit()
+
+        try:
+            if cap.isOpened():
+                return cap
+            else:
+                print("Failed to load")
+                sys.exit()
+        except:
+            print('Failed to load')
 
     def start(self):
         cap = self.capture()
@@ -37,18 +41,13 @@ class Recognize(HaarClassifier):
                 self.haarClassifier.detectFace(grayScale, img)
 
                 cv2.imshow('img', img)
-
-                if (len(faces) != 0):
-                    cv2.imwrite('face_image_%d.png' % count)
-                    count += 1
-
                 k = cv2.waitKey(1)
                 if k == ord('q'):
                     break
-                    
+
         cap.release()
         cv2.destroyAllWindows()
 
-if __name__ == '__main__':
-    recognize = Recognize()
-    recognize.start()
+if __name__ == "__main__":
+    recognition = Recognition()
+    recognition.start()
